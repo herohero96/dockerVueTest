aed9184d0068dc06d205a12eaee3304e8bc26820
zhiyong.liu <zhiyong.liu@getech.cn>
2020-07-03 14:58:02 +0800

    codker

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="hellow docker"/>
+    <HelloWorld msg="hellow docker2"/>
   </div>
 </template>
 

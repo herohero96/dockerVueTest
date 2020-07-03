e1915f5698520162b600b0fe00a3c87a60ba7dd4
zhiyong.liu <zhiyong.liu@getech.cn>
2020-07-03 15:03:55 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="hellow docker2"/>
+    <HelloWorld msg="hellow docker22"/>
   </div>
 </template>
 
